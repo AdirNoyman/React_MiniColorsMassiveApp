aa1e2ee88e3ffdda2b3a0b54696dad5921eabd02
Adir Noyman <45500652+AdirNoyman@users.noreply.github.com>
2019-09-28 23:26:02 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Palette from './Palette';
+import PaletteList from './PaletteList';
+import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
+import { palette } from '@material-ui/system';
+
+class App extends Component {
+
+  findPalette(id) {
+    return seedColors.find(palette => palette.id === id);
+  }
+
+  render() {
+
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <PaletteList palettes={seedColors} />} />
+        <Route exact path="/palette/:id" render={routeProps => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />} />
+      </Switch>
+
+
+      //{/* <div>
+      //  <Palette palette={generatePalette(seedColors[4])} />
+      //</div> */}
+    );
+  }
+
+}
+
+export default App;
---
+++ src/Palette.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+import NavBar from './NavBar';
+import './Palette.css';
+
+class Palette extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { level: 500, format: "hex" };
+        this.changeLevel = this.changeLevel.bind(this);
+        this.changeColorFormat = this.changeColorFormat.bind(this);
+    }
+
+    changeLevel(level) {
+
+        this.setState({ level });
+
+    }
+
+    changeColorFormat(value) {
+
+        this.setState({ format: value });
+
+    }
+
+    render() {
+
+        const { colors, paletteName, emoji } = this.props.palette;
+        const { level, format } = this.state;
+        const colorBoxes = colors[level].map(color => <ColorBox background={color[format]} name={color.name} key={color.id} />);
+        return (
+            <div className="Palette">
+                <NavBar level={level} changeLevel={this.changeLevel} handelChange={this.changeColorFormat} />
+
+                <div className="Palette-colors">
+                    {colorBoxes}
+                </div>
+                <footer className="Palette-footer">
+                    {paletteName}<span className="emoji">{emoji}</span>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default Palette;
